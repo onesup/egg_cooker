84f7be689078cfcb4891378ff618e3059704ecc5
Wonsup Lee <daul0212@gmail.com>
2013-07-05 15:26:18 +0900

    capify 설정

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EggCooker::Application.config.secret_token = 'e6fd91d9edd53577537df6d27cb5f3ac7c0d15bc777fee9730e160102f8e542f8920cc454032fc58345bf62b49342f162fdc1f0922fe297143fa12dffeee2e30'
---
+++ config/unicorn.rb
@@ -0,0 +1,14 @@
+root = "/home/daul/egg_cooker/current"
+working_directory root
+pid "#{root}/tmp/pids/unicorn.pid"
+stderr_path "#{root}/log/unicorn.err.log"
+stdout_path "#{root}/log/unicorn.out.log"
+ 
+# change the YOUR_APP_NAME to your application name
+listen "/tmp/unicorn.egg_cooker.sock"
+worker_processes 2
+timeout 30
+
+before_exec do |server|
+  ENV["BUNDLE_GEMFILE"] = "#{root}/Gemfile"
+end
